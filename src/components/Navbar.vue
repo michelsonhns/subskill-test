ca4a265870ff1b6b09031b184618b0532eda843e
michelsonhnsn <michelson@poucedigital.com>
2021-08-22 02:30:55 +0200

    add toggle in the navbar responsive

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -4,6 +4,7 @@
       <div class="nav-brand">
         <img src="../assets/logos/logo-avocats-de-france.png" alt="">
       </div>
+      <button class="navbar-toggler" aria-label="Toggle navigation"><i class="fas fa-bars"></i></button>
       <ul>
         <li class="nav-item">Toute l’actu</li>
         <li class="nav-item">Le club</li>
@@ -22,32 +23,45 @@
 </script>
 
 <style lang="stylus" scoped>
-nav
-  display flex
-  align-items center;
-  padding: 16px 282px 17px 48px;
-  border-bottom solid 1px #efefef
-
-
-nav ul
-  display flex
-  margin 0 auto
-
-nav ul li
-  padding-left 30px
-
-ul 
-  list-style: none
-
-
-ul li::before 
-  content: "•"
-  margin-right 30px
-
-nav ul li:first-child::before 
-  content: ""
-  
 img
   width 75px
+
+nav
+  display flex
+  justify-content space-between
+  align-items center;
+  padding: 16px
+  border-bottom solid 1px #efefef
+
+  ul
+    display none
+  
+  button
+    background-color #FFF
+    border none
+    font-size 1.8em
+
+@media (min-width: 1131px)
+  nav
+    padding: 16px 282px 17px 48px;
+    button
+      display none
+  nav ul
+    display flex
+    margin 0 auto
+
+  nav ul li
+    padding-left 30px
+
+  ul 
+    list-style: none
+
+
+  ul li::before 
+    content: "•"
+    margin-right 30px
+
+  nav ul li:first-child::before 
+    content: ""
 
 </style>
