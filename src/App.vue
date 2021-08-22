1d7f68c0f22b78777ac7eb208963bf821dcd4083
michelsonhnsn <michelson@poucedigital.com>
2021-08-22 23:33:05 +0200

    refactoring, add content in a aside

--- src/App.vue
+++ src/App.vue
@@ -4,33 +4,36 @@
       <Navbar/>
     </header>
     <main>
-      <div class="container">
-        <section class="cover__container">
+      <section class="cover__container">
+        <div class="container">
           <Card 
-              v-for="article in covers" :key="article.id"
-              :title="article.title"
-              :daytime="article.daytime"
-              :description="article.description"
-              :src="article.src"
-              :tag="article.tag"
-              :class_description="article.class_description"
-              :class_title="article.class_title"
-            />
-        </section>
-        <section class="articles-container">
-          <div class="article-group">
-            <Card 
-              v-for="article in articles" :key="article.id"
-              :title="article.title"
-              :daytime="article.daytime"
-              :description="article.description"
-              :src="article.src"
-              :tag="article.tag"
-              :alt="article.alt"
-            />
-          </div>
-        </section>
-      </div>
+            v-for="article in covers" :key="article.id"
+            :title="article.title"
+            :daytime="article.daytime"
+            :description="article.description"
+            :src="article.src"
+            :tag="article.tag"
+            :class_description="article.class_description"
+            :class_title="article.class_title"
+          />
+        </div>
+        
+      </section>
+
+      <section class="articles__container">
+        <div class="article__group container">
+          <Card 
+            v-for="article in articles" :key="article.id"
+            :title="article.title"
+            :daytime="article.daytime"
+            :description="article.description"
+            :src="article.src"
+            :tag="article.tag"
+            :alt="article.alt"
+          />
+        </div>
+      </section>
+      
       <section class="members__container">
         <div class="container">
           <h2 class="section__title">Nos présidents d’honneur</h2>
@@ -60,16 +63,23 @@
             :title="newsletter.title"
             :description="newsletter.description"
             :positioning="newsletter.positioning"
-            :src="newsletter.src">
+            :src="newsletter.src"
+            :daytime="newsletter.daytime">
               <Button title="Découvrir l’article" btnclass="yellow"/>
             </Card>
             
           </div>
-          <h2 class="section__title">Nos présidents d’honneur</h2>
+          <h2 class="section__title">Le club des juristes</h2>
+          <aside>
+            <p>Créé en 2007, le Club des juristes est <span class="bold">le premier think tank juridique français</span>. Il s’agit d’un lieu indépendant de réflexion sur la place du droit en France. <span class="bold">Le « Club » a pour objectif de nourrir le débat public</span> en formulant des propositions concrètes dans le domaine du droit et de la justice. C’est notamment le travail des commissions – justice, fiscalité, environnement, constitutions et institutions, Europe, et commissions dites « ad hoc » – qu’il réunit en son sein et qui aboutissent à des recommandations innovantes, utiles aux décideurs publics. Pour ce faire, <span class="bold">le Club des juristes réunit des professionnels du droit reconnus issus d’horizons divers : avocats, magistrats, notaires, universitaires, représentants d’entreprises, etc</span>.</p>
+            <Button
+              title="En savoir plus"
+               btnclass="white"/>
+          </aside>
         </div>
       </section>
 
-      <section class="partners-container">
+      <section class="partners__container">
         <div class="container">
           <h2 class="section__title">Nos partenaires</h2>
           <Partners/>
@@ -86,7 +96,7 @@
         <Carousel/>
       </section>
 
-      <section class="contact-container">
+      <section class="contact__container">
         <div class="container">
           <h2 class="section__title">Nous contacter</h2>
           <span class="help-text">Afin de traiter votre demande dans les plus brefs délais, nous vous invitons à utiliser le formulaire ci-dessous.</span>
@@ -176,6 +186,7 @@ export default {
           description: "Le recours aux sanctions internationales – comme celles adoptées en 2021 par les Etats-Unis contre des entreprises chinoises spécialisées dans la défense et les technologies de surveillance1 – ne cesse de croître et, dès lors, d’occuper l’actualité de l’arbitrage international.",
           positioning: "horizontal",
           src: require("@/assets/images/video1.png"),
+          daytime: "10 août 2021",
 
         },
         {
@@ -183,6 +194,7 @@ export default {
           description: "Le recours aux sanctions internationales – comme celles adoptées en 2021 par les Etats-Unis contre des entreprises chinoises spécialisées dans la défense et les technologies de surveillance1 – ne cesse de croître et, dès lors, d’occuper l’actualité de l’arbitrage international.",
           positioning: "horizontal img-right",
           src: require("@/assets/images/video2.png"),
+          daytime: "10 août 2021",
 
         }
       ]
@@ -192,11 +204,13 @@ export default {
 </script>
 
 <style lang="stylus">
-
 body 
   margin 0
   font-size 100%
-  font-family: 'Poppins', sans-serif;
+  font-family 'Poppins', sans-serif
+
+.bold
+  font-weight bold
 
 .container {
   width 95%
@@ -206,59 +220,54 @@ body
 .section__title
   font-size: 3.43em;
   margin-bottom 50px
-  font-weight: normal;
+  font-weight normal
+  font-family "Seshat", sans-serif
+
 .cta 
   text-align center
 
 .cover__container
+  background url("./assets/icons/round-black.png") bottom 285px right 0 / 110px no-repeat
   .card 
     width 100%
     img
       max-width 100%
 
-.article-group
+.article__group
   .card 
     img
       max-width 100%
 
-  
 .members__container
+  background-image url("./assets/icons/round-bicolor.png")
+  background-repeat no-repeat
+  background-position: bottom -102px right 20px;
+  background-size: 190px;
   padding: 70px 0;
-  .card__title
-    color #FFFFFF !important
   background-color #ffb563
+  font-stretch normal
   color #FFFFFF
-  .section__title
-    font-size 
-
-.members__container .card__description
-  color #FFFFFF !important
-
-.members__container button
-  width: 299px;
-  height: 64px;
-  margin: 70px 54px 0 55px;
-  padding: 20px 15px;
-  background-color: #fff;
-  border none
-  margin 0 auto
-
+  .card__description, .card__title
+    color #FFFFFF !important
 
 .news__container
-  padding: 150px 0;
+  padding: 39px 0;
   background-color: #f2f2f2;
   .card 
     img
       max-width 100%
 
-.partners-container 
-  padding-top 337px
+.news__container
+  background  url("./assets/icons/round-bicolor.png")top -92px right 20px / 190px no-repeat,url("./assets/icons/round-green.png") bottom -127px left 30px / 218px no-repeat
+  background-color #F2F2F2
+  padding-bottom 150px
 
 .help-text
   font-family Poppins
   font-size 0.87em
   color #2d2d2d
-.contact-container
+
+.contact__container
   p
     margin: 15px 0 50px;
     font-family: Poppins;
@@ -267,12 +276,21 @@ body
     font-style: italic;
     color: #2d2d2d;
 
+aside
+  background #b3dec1
+  padding 30px;
 
+.partners__container
+  padding-top 100px
+  margin-bottom 160px
+  background url("./assets/icons/round-green.png") top -115px left 30px / 218px no-repeat
+
+// Desktop
 @media (min-width: 1131px)
   .container {
     width 80%
   }
-  .article-group
+  .article__group
     display flex
     justify-content space-between
     .card 
@@ -280,6 +298,29 @@ body
       img
         max-width 100%
   
+  .cover__container
+    margin-top 51px
+    background-position bottom 171px right 100px
+    background-size 193px
+
+  .news__container
+    padding: 150px 0;
+    background url("./assets/icons/round-bicolor.png") top -140px right 20px / 290px no-repeat, url("./assets/icons/round-green.png") bottom -127px left 30px / 218px no-repeat
+    background-color #F2F2F2
+
+  .partners__container
+    margin-bottom 160px
+    padding-top 337px
+    background url("./assets/icons/round-green.png") top -116px left 30px / 218px no-repeat
+    .cta
+      margin-top 70px
+
+  .members__container
+    background-image url("./assets/icons/round-bicolor.png")
+    background-repeat no-repeat
+    background-position: bottom -155px right 20px;
+    background-size: 290px;
+
   .members__wrap
     display flex
     justify-content space-between
@@ -293,7 +334,18 @@ body
         text-decoration  underline
       img:hover
         filter grayscale(0)
+  
   .videos__section
     padding: 70px 49px 70px 46px;
     background-color: #f2f2f2;
+
+  aside
+    padding 50px 57px 50px 59px;
+    width: 40%;
+    position absolute
+    right 14%
+  
+  .contact__container
+    margin-bottom 50px
+
 </style>
